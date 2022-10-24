59db8590aeb3ee7cf2d1473add48ac1d8286fa71
Your Name <duc123456789th@gmail.com>
2022-10-24 21:47:11 +0700

    Add

--- form/src/main/java/com/example/form/repository/IUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.form.repository;
-
-import com.example.form.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<User,Integer> {
-}
--- form/src/main/java/com/example/form/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.form.service;
-
-import com.example.form.model.User;
-
-public interface IUserService {
-    void save(User user);
-}
