c74f4743d0af078beb817df125f376f6ca31c539
Your Name <duc123456789th@gmail.com>
2022-11-01 21:38:37 +0700

    Add

--- product/src/main/java/product/repository/IProductRepository.java
+++
@@ -1,9 +0,0 @@
-package product.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-import product.model.Product;
-
-@Repository
-public interface IProductRepository extends PagingAndSortingRepository<Product, Long> {
-}
