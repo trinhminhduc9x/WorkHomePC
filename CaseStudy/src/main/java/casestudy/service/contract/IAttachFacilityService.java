aa06205b60a541cc2ed56b30dd0f4a5ddcf26553
Your Name <duc123456789th@gmail.com>
2022-12-13 21:55:24 +0700

    Add

--- CaseStudy/src/main/java/casestudy/service/contract/IAttachFacilityService.java
+++
@@ -1,9 +0,0 @@
-package casestudy.service.contract;
-
-import casestudy.model.contract.AttachFacility;
-
-import java.util.List;
-
-public interface IAttachFacilityService {
-    List<AttachFacility> findListAll();
-}
