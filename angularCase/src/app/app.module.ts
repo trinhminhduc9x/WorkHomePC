697639ecb442715f0f2fe12357bd559672eb9169
Your Name <duc123456789th@gmail.com>
2022-12-30 05:57:41 +0700

    add

--- angularCase/src/app/app.module.ts
+++ angularCase/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CutomerListComponent } from './cutomer-list/cutomer-list.component';
+import { ProductListComponent } from './product/product/product-list/product-list.component';
+import { ProductCreateComponent } from './product/product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CutomerListComponent
+    CutomerListComponent,
+    ProductListComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
