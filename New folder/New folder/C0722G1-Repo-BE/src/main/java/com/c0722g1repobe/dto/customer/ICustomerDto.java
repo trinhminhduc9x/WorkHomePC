4eee52d5e4d8ce512f70b2c4a5589b4c684a9807
Your Name <duc123456789th@gmail.com>
2023-02-04 05:59:34 +0700

    add

---
+++ New folder/New folder/C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/dto/customer/ICustomerDto.java
@@ -0,0 +1,20 @@
+package com.c0722g1repobe.dto.customer;
+
+public interface ICustomerDto {
+    String getNameCustomer();
+    String getEmailCustomer();
+    String getAddressCustomer();
+    String getIdCardCustomer();
+    String getCodeCustomer();
+    Integer getGenderCustomer();
+
+    String getDateOfBirth();
+    String getPhoneCustomer1();
+
+    String getPhoneCustomer2();
+    String getUsernameAccount();
+    String getEncryptPassword();
+    String getPasswordConfirm();
+
+}
+
